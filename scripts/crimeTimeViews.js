26ab384ec117893344733530a85971f27f8e5db0
Matthias Miller <Matthias.Miller229@gmail.com>
2016-05-09 19:32:05 +0200

    Data Added to Timeline and Matrix
    Transitions for showing categories!
    
    TODO: linking to matrix with highlight
    onhover
    Buttons besser darstellen

--- scripts/crimeTimeViews.js
+++ scripts/crimeTimeViews.js
@@ -1,6 +1,4 @@
 
-
-
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
@@ -13,6 +11,7 @@ var timeScale = d3.time.scale()
     .range([0,width])
     .domain(timeRange);
 
+var svg;
 var crimeTypes = [
     "allCrimes",
     "violence_and_sex",
@@ -42,138 +41,127 @@ var years = ["y2010","y2011","y2012","y2013","y2014","y2015","y2016"];
 var months = ["m01","m02","m03","m04","m05","m06","m07","m08","m09","m10","m11","m12"];
 var NUM_MONTHS = 63;
 var CLUSTERINGCOLORS = ['red', 'aqua', 'blue', 'fuchsia', 'gray', 'green', 'lime',
-    'maroon', 'navy', 'olive', 'silver', 'teal', 'yellow', 'blueviolet',
-    'burlywood', 'cadetblue', 'chocolate', 'chartreuse', 'darkgoldenrod',
-    'darkmagenta', 'darkkhaki', 'darkorange', 'darkolivegreen', 'darkgreen',
-    'darkcyan'];
+    'maroon', 'darkorange', 'olive', 'silver', 'darkolivegreen', 'blueviolet',
+    'burlywood', 'darkcyan', 'chocolate', 'darkgoldenrod',
+    'darkmagenta', 'darkkhaki' , 'darkgreen'];
 
+
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 var svg1;
 
-var crimes = {
-    "aggregatedCrimesByMonth" : [
-        {"date":"2011-02", "allCrimes" : "300", "theft" : "20", "robbery": "70"},
-        {"date":"2011-03", "allCrimes" : "400", "theft" : "40", "robbery": "70"},
-        {"date":"2011-04", "allCrimes" : "200", "theft" : "50", "robbery": "70"},
-        {"date":"2012-02", "allCrimes" : "300", "theft" : "20", "robbery": "70"},
-        {"date":"2012-03", "allCrimes" : "400", "theft" : "40", "robbery": "70"},
-        {"date":"2012-04", "allCrimes" : "200", "theft" : "50", "robbery": "70"},
-        {"date":"2012-06", "allCrimes" : "300", "theft" : "20", "robbery": "30"},
-        {"date":"2013-03", "allCrimes" : "400", "theft" : "40", "robbery": "70"},
-        {"date":"2013-04", "allCrimes" : "200", "theft" : "50", "robbery": "70"},
-        {"date":"2013-06", "allCrimes" : "300", "theft" : "20", "robbery": "70"},
-        {"date":"2014-03", "allCrimes" : "400", "theft" : "40", "robbery": "70"},
-        {"date":"2014-04", "allCrimes" : "200", "theft" : "50", "robbery": "70"},
-        {"date":"2014-07", "allCrimes" : "300", "theft" : "20", "robbery": "120"},
-        {"date":"2014-08", "allCrimes" : "400", "theft" : "40", "robbery": "70"},
-        {"date":"2015-04", "allCrimes" : "200", "theft" : "50", "robbery": "70"},
-        {"date":"2015-06", "allCrimes" : "300", "theft" : "20", "robbery": "70"},
-        {"date":"2015-08", "allCrimes" : "400", "theft" : "40", "robbery": "70"},
-        {"date":"2015-09", "allCrimes" : "200", "theft" : "50", "robbery": "70"}
-    ],
-    "crimesTotal" : [
-        {"type" : "allCrimes","sum" : "2000", "min" : "200", "max": "500"},
-        {"type" : "theft","sum":"800", "min" : "20", "max": "50"},
-        {"type" : "robbery","sum" : "1200", "min" : "20", "max": "120"}]
-};
+var crimes;
 
-var months = ["2010-12"];
-for(var i = 2011; i < 2016; i++){
-    for(var j = 1; j < 13; j++){
+function showViews(){
+    createCrimeCategoryButtons();
+    d3.json("https://raw.githubusercontent.com/FabianSperrle/InfoVisIIPreProcessing/master/crimeAggregates.json", function(error, data) {
+        if (error) throw error;
+        crimes = data;
+        timelineView();
+        matrixView();
+    });
+}
 
+function createCrimeCategoryButtons(){
+    d3.select("#crimeCheckboxes").selectAll("input")
+        .data(crimeTypes)
+        .enter()
+        .append('label')
+        .attr('for', function(d,i){return 'a'+i;})
+        .style("color",function (d,i){return CLUSTERINGCOLORS[i]})
+        .text(function(d,i){return d;})
+        .append("input")
+        .attr("type", "checkbox")
+        .attr("id", function(d,i){return "category_"+i;})
+        .attr("onClick",function(d,i){return "toggleCheckboxesOfCrimes("+i+")"})
+        .attr("for", function(d,i){return "a"+i;});
+    d3.selectAll('input').property('checked', false);
+    for(var i = 0; i < activeCrimesCheckBoxes.length; i++){
+        d3.select("#category_"+i).property("checked", activeCrimesCheckBoxes[i]);
     }
 }
 
-var crimeTimeMatrix;
-function matrixView() {
-    var margin = {top: 50, right: 0, bottom: 100, left: 30},
-        width = 960 - margin.left - margin.right,
-        height = 430 - margin.top - margin.bottom,
-        gridSize = Math.floor(width / NUM_MONTHS),
-        legendElementWidth = gridSize * 2,
-        buckets = 9,
-        colors = ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"], // alternatively colorbrewer.YlGnBu[9]
-
-    crimeTimeMatrix = d3.select("#crimeTimeMatrix").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    //
-
-    var monthIndex = 0;
-
-    function preprocess (){
-        for(var j = 0; j < years.length; j++){
-            for(var k = 0; k < months.legth; k++){
-                monthIndex++;
-                for(var i = 0; i < crimeTypes.length; i++){
-                    var val = crimes[years[j]][months[k]][crimeTypes[i]];
-                    log(val);
-                    crimeTimeMatrix.append("rect")
-                        .attr("x", gridSize * j)
-                        .attr("y", gridSize * i)
-                        .attr("rx", 5)
-                        .attr("ry", 5)
-                        .attr("width", gridSize)
-                        .attr("height", gridSize)
-                        .attr("class", "field bordered")
-                        .style("opacity", function(){
-
-                        })
-                        .style("fill", "red");
-                }
-            }
-        }
-    }
-
-    for(var j = 0; j < years.length; j++){
-        for(var k = 0; k < months.legth; k++){
-            monthIndex++;
-            for(var i = 0; i < crimeTypes.length; i++){
-                crimeTimeMatrix.append("rect")
-                    .attr("x", gridSize * j)
-                    .attr("y", gridSize * i)
-                    .attr("rx", 5)
-                    .attr("ry", 5)
-                    .attr("width", gridSize)
-                    .attr("height", gridSize)
-                    .attr("class", "field bordered")
-                    .style("opacity", function(){
-
-                    })
-                    .style("fill", "red");
-            }
-        }
-    }
-
+function toggleCheckboxesOfCrimes(checkboxID){
+    activeCrimesCheckBoxes[checkboxID] = d3.select("#category_"+checkboxID).property("checked");
+    toggleTimeviewLines(checkboxID);
+    resizeTimeLine(crimes);
 }
 
+
+function getCrimeData(crimeType, data){
+    var returndata = [];
+    for (var i = 0; i <= years.length - 1; i++) {
+        for (var j = 0; j <= months.length - 1; j++) {
+            if( typeof data[years[i]] === 'undefined' || typeof data[years[i]][months[j]] === 'undefined' ) {
+
+            } else {
+                var date = years[i].slice(1,5) + "-" + months[j].slice(1,3);
+                //log(date);
+                var obj = {"date": date, "value": data[years[i]][months[j]][crimeType]};
+                returndata.push(obj);
+            }
+
+        }
+    }
+    return returndata;
+}
+
+var plotCrimePath = d3.svg.line()
+    .x(function(d) { return x(dateFormat.parse(d.date)); })
+    .y(function(d) { return y(d.value); });
+function plotTimeviewLines(data){
+    for(var i = 0; i < crimeTypes.length; i++){
+
+        svg.append("path")
+            .attr("id", crimeTypes[i])
+            .datum(getCrimeData(crimeTypes[i], data))
+            .attr("class", "line")
+            .attr("d", plotCrimePath)
+            .attr("stroke-width",10)
+            .attr("stroke",CLUSTERINGCOLORS[i]);
+        if(!activeCrimesCheckBoxes[i]){
+            d3.select("#"+crimeTypes[i]).attr("visibility","hidden");
+        }
+    }
+    resizeTimeLine(data);
+}
+
+function toggleTimeviewLines(crimeLineID){
+    if(activeCrimesCheckBoxes[crimeLineID]){
+        d3.select("#"+crimeTypes[crimeLineID]).attr("visibility","block");
+    } else {
+        d3.select("#"+crimeTypes[crimeLineID]).attr("visibility","hidden");
+    }
+}
+
+function resizeTimeLine(data){
+    var maxValue = 0;
+    for(var i = 0; i < crimeTypes.length; i++) {
+        if (activeCrimesCheckBoxes[i]) {
+            if (countMax[i] > maxValue)
+                maxValue = countMax[i];
+        }
+    }
+    y.domain([0,maxValue+50]);
+    for(i = 0; i < crimeTypes.length; i++){
+        d3.select("#"+crimeTypes[i]).transition().duration(750).attr("d",plotCrimePath(getCrimeData(crimeTypes[i], data)));
+    }
+    svg.select(".y.axis") // change the y axis
+        .transition().duration(750)
+        .call(yAxis);
+}
 
 function timelineView(){
-
-    var x = d3.time.scale()
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-        .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var plotCrimePath = d3.svg.line()
-        .x(function(d) { return x(dateFormat.parse(d.date)); })
-        .y(function(d) { return y(d.value); });
-
-
     svg1 = d3.select("#timelineView").append("svg:svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom);
-    var svg = svg1.append("g")
+    svg = svg1.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     d3.json("https://raw.githubusercontent.com/FabianSperrle/InfoVisIIPreProcessing/master/crimeAggregates.json", function(error, data) {
@@ -182,27 +170,6 @@ function timelineView(){
         crimes = data;
         years = ["y2010","y2011","y2012","y2013","y2014","y2015","y2016"];
         months = ["m01","m02","m03","m04","m05","m06","m07","m08","m09","m10","m11","m12"];
-
-
-        function getCrimeData(crimeType){
-
-            var returndata = [];
-
-            for (var i = 0; i <= years.length - 1; i++) {
-                for (var j = 0; j <= months.length - 1; j++) {
-                    if( typeof data[years[i]] === 'undefined' || typeof data[years[i]][months[j]] === 'undefined' ) {
-
-                    } else {
-                        var date = years[i].slice(1,5) + "-" + months[j].slice(1,3);
-                        //log(date);
-                        var obj = {"date": date, "value": data[years[i]][months[j]][crimeType]};
-                        returndata.push(obj);
-                    }
-
-                }
-            }
-            return returndata;
-        }
 
         x.domain(timeRange);
         y.domain([0,500]);
@@ -222,18 +189,7 @@ function timelineView(){
             .style("text-anchor", "end")
             .text("Number of Crimes");
 
-
-        for(var i = 0; i < crimeTypes.length; i++){
-            if(activeCrimesCheckBoxes[i]){
-                svg.append("path")
-                    .attr("id", crimeTypes[i])
-                    .datum(getCrimeData(crimeTypes[i]))
-                    .attr("class", "line")
-                    .attr("d", plotCrimePath)
-                    .attr("stroke-width",10)
-                    .attr("stroke",CLUSTERINGCOLORS[i]);
-            }
-        }
+        plotTimeviewLines(data);
 
         // SLIDER
         var _dragSliderLine;
@@ -301,6 +257,69 @@ function timelineView(){
     }
 }
 
+
+var crimeTimeMatrix;
+var countMax;
+var countMin;
+function matrixView() {
+    var margin = {top: 50, right: 0, bottom: 100, left: 30},
+        width = 960 - margin.left - margin.right,
+        height = 430 - margin.top - margin.bottom,
+        gridSize = Math.floor(width / NUM_MONTHS),
+        legendElementWidth = gridSize * 2,
+        buckets = 9,
+        colors = ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"]; // alternatively colorbrewer.YlGnBu[9]
+
+    crimeTimeMatrix = d3.select("#crimeTimeMatrix").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    //
+
+    countMax = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    countMin = [Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE];
+    for(var j = 0; j < years.length; j++){
+        for(var k = 0; k < months.length; k++){
+            for(var i = 0; i < crimeTypes.length; i++){
+                //log(years[j] + " - " + months[k] + " " + crimeTypes[i]);
+                if(!(typeof crimes[years[j]] === 'undefined' || typeof crimes[years[j]][months[k]] === 'undefined')) {
+                    var val = crimes[years[j]][months[k]][crimeTypes[i]];
+                    if (typeof  val !== "undefined"){
+                        if (countMax[i] < val) countMax[i] = val;
+                        if (countMin[i] > val) countMin[i] = val;
+                    }
+                }
+            }
+        }
+    }
+
+    var monthIndex = 0;
+    for(var j = 0; j < years.length; j++){
+        for(var k = 0; k < months.length; k++){
+            if(!(typeof crimes[years[j]] === 'undefined' || typeof crimes[years[j]][months[k]] === 'undefined')) {
+                for(var i = 0; i < crimeTypes.length; i++){
+                    crimeTimeMatrix.append("rect")
+                        .attr("x", gridSize * monthIndex)
+                        .attr("y", gridSize * i)
+                        .attr("rx", 5)
+                        .attr("ry", 5)
+                        .attr("width", gridSize)
+                        .attr("height", gridSize)
+                        .attr("class", "field bordered")
+                        .style("opacity", function () {
+                            var val = crimes[years[j]][months[k]][crimeTypes[i]];
+                            return (val-countMin[i])/(countMax[i]-countMin[i]);
+                        })
+                        .style("fill", CLUSTERINGCOLORS[i]);
+                }
+                monthIndex++;
+            }
+        }
+    }
+}
+
+
 function printDate(){
     var date = getDate();
     d3.select("#sliderDate").node().innerHTML = date[0] + " " + date[1];
@@ -319,3 +338,4 @@ function getDate(){
 function log(text){
     console.log(text);
 }
+
