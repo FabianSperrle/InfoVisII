cb6498e243467d646eedca132d26c640e4e60033
Matthias Kraus <kraus.matze@googlemail.com>
2016-05-12 23:06:51 +0200

    test make visualizations dragable - test jqueryui

---
+++ TEST-Dragable/scripts/dataController.js
@@ -0,0 +1,207 @@
+function DataController() {
+	telegraph(this);
+	this.all = {};
+	this.crimes = {};
+	this.crimesByType = {};
+	this.filtered = {};
+	this.crimeAggregates = {};
+
+	this.crimeTypes = {
+		allCrimes: {
+			visibility: 1,
+			verboseName: 'All Crimes',
+			color: 'red',
+		},
+		violence_and_sex: {
+			visibility: 0,
+			verboseName: 'Violence and sexual offences',
+			color: 'aqua',
+		},
+		other_theft: {
+			visibility: 1,
+			verboseName: 'Other theft',
+			color: 'blue',
+		},
+		burglary: {
+			visibility: 0,
+			verboseName: 'Burglary',
+			color: 'fuchsia',
+		},
+		violent_crime: {
+			visibility: 0,
+			verboseName: 'Violent crime',
+			color: 'gray',
+		},
+		bicycle_theft: {
+			visibility: 0,
+			verboseName: 'Bicycle theft',
+			color: 'green',
+		},
+		anti_social_behaviour: {
+			visibility: 0,
+			verboseName: 'Anti-social behaviour',
+			color: 'lime',
+		},
+		other_crime: {
+			visibility: 0,
+			verboseName: 'Other crime',
+			color: 'maroon',
+		},
+		shoplifting: {
+			visibility: 0,
+			verboseName: 'Shoplifting',
+			color: 'darkorange',
+		},
+		drugs: {
+			visibility: 0,
+			verboseName: 'Drugs',
+			color: 'olive',
+		},
+		criminal_damage_and_arson: {
+			visibility: 0,
+			verboseName: 'Criminal damage and arson',
+			color: 'silver',
+		},
+		vehicle_crime: {
+			visibility: 0,
+			verboseName: 'Vehicle crime',
+			color: 'darkolivegreen',
+		},
+		theft_from_the_person: {
+			visibility: 0,
+			verboseName: 'Theft from the person',
+			color: 'blueviolet',
+		},
+		public_disorder_weapons: {
+			visibility: 0,
+			verboseName: 'Public disorder and weapons',
+			color: 'burlywood',
+		},
+		public_order: {
+			visibility: 0,
+			verboseName: 'Public order',
+			color: 'darkcyan',
+		},
+		robbery: {
+			visibility: 0,
+			verboseName: 'Robbery',
+			color: 'chocolate',
+		},
+		possesion_of_weapons: {
+			visibility: 0,
+			verboseName: 'Possession of weapons',
+			color: 'darkgoldenrod',
+		},
+	};
+	this.visibleVerboseCrimeTypes = ["Other theft"];
+}
+
+/**
+ * Initializes the filters by setting everything to visible.
+ * @return {[type]}
+ */
+DataController.prototype.initializeFilters = function() {
+	this.crimes = crossfilter(this.all);
+	this.crimesByType = this.crimes.dimension(function(d) {
+		return d.crimetype;
+	});
+	this.filtered = this.crimesByType.filterAll().top(Infinity);
+	this.visibleVerboseCrimeTypes = ["Violence and sexual offences", "Other theft", "Burglary", "Violent crime", "Bicycle theft", "Anti-social behaviour", "Other crime", "Shoplifting", "Drugs", "Criminal damage and arson", "Vehicle  crime", "Theft from the person", "Public disorder and weapons", "Public order", "Robbery", "Possession of weapons"];
+
+	data.emit('filtered');
+};
+
+/**
+ * Changes the visibility of a given crime type. 
+ * @param  {string} The crime type to change
+ * @return {[type]}
+ */
+DataController.prototype.toggleVisibilityFlag = function(type) {
+	var old = this.crimeTypes[type].visibility;
+	this.crimeTypes[type].visibility = (old + 1) % 2;
+	var current = this.crimeTypes[type].visibility;
+
+	// Handle special case
+	if (type == "allCrimes") {
+		this.visibleVerboseCrimeTypes = [];
+		if (current > old) {
+			// Set everything to visible
+			for (var key in this.crimeTypes) {
+				this.visibleVerboseCrimeTypes.push(this.crimeTypes[key].verboseName);
+			}
+		} else {
+			for (var key in this.crimeTypes) {
+				if (this.crimeTypes[key].visibility == 1) {
+					this.visibleVerboseCrimeTypes.push(this.crimeTypes[key].verboseName);
+				}
+			}
+		}
+		return; 
+	}
+
+	// Type was invisible, is visible now
+	// Add verbose name to the visible list
+	if (current > old) {
+		this.visibleVerboseCrimeTypes.push(this.crimeTypes[type].verboseName);
+	} else {
+		// Find the correct index
+		var index = this.visibleVerboseCrimeTypes.indexOf(this.crimeTypes[type].verboseName);
+		// And remove it from the array
+		this.visibleVerboseCrimeTypes.splice(index, 1);
+	}
+};
+
+DataController.prototype.toggleFilter = function(type) {
+	this.toggleVisibilityFlag(type);
+
+	// Apply all filters
+	this.filtered = this.crimesByType.filter(function(d) {
+		if (data.visibleVerboseCrimeTypes.indexOf(d) >= 0)
+			return true;
+		return false;
+	}).top(Infinity);
+
+	data.emit('filtered');
+};
+
+DataController.prototype.mapFilterKeyword = function(key) {
+	return this.CrimeTypes[key].verboseName;
+};
+
+DataController.prototype.getCrimeTypes = function() {
+	var names = [];
+	for (var key in this.crimeTypes)  {
+		names.push(key);
+	}
+
+	return names;
+}
+
+DataController.prototype.getVerboseCrimeName = function(crime) {
+	return this.crimeTypes[crime].verboseName;
+}
+
+DataController.prototype.getCrimeColor = function(crime) {
+	return this.crimeTypes[crime].color;
+}
+
+var data = new DataController();
+
+
+d3.json("https://raw.githubusercontent.com/FabianSperrle/InfoVisIIPreProcessing/master/DataComplete/crimes.json", function(error, json) {
+	if (error) throw error;
+
+	data.all = json;
+	data.filtered = json;
+	data.emit('loadAll');
+});
+
+d3.json("https://raw.githubusercontent.com/FabianSperrle/InfoVisIIPreProcessing/master/crimeAggregates.json", function(error, json) {
+	if (error) throw error;
+
+	data.crimeAggregates = json;
+	data.emit('loadAggregates');
+});
+
+data.on('loadAll', data.initializeFilters);
+data.on('toggle', data.toggleFilter);
---
+++ TEST-Dragable/scripts/pointCluster.js
@@ -0,0 +1,43 @@
+var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//var tiles = L.tileLayer('https://api.apbox.com/styles/v1/fabiansperrle/cio2xydhi003dbzm14yr5xu17/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmFiaWFuc3BlcnJsZSIsImEiOiJjaW51NXBlOXowMG13dzltMndzdHI4b3gwIn0.-KwI70EkNL2Ni6YWkXfKsQ', {
+		maxZoom: 20,
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ, Tiles from Mapbox'
+	})
+var latlng = L.latLng(51.513819, -0.098361);
+var map = L.map('map', { center: latlng, zoom: 13, layers: [tiles] });
+
+var markers = L.markerClusterGroup({ 
+	chunkedLoading: true, 
+	maxClusterRadius: 80,
+});
+
+function updateClusters() {
+	map.removeLayer(markers);
+
+	markers = L.markerClusterGroup({ 
+		chunkedLoading: true, 
+		maxClusterRadius: 80,
+	});
+	var markerList = [];
+
+	for (var i = 0; i < data.filtered.length; i++) {
+		var a = data.filtered[i];
+		var title = a.crimetype;
+
+		var lat = a.latitude;
+		var lng = a.longitude;
+
+		if (lat == "" || lng == "" || lat == "Latitude" || lng == "Longitude") {
+			continue;
+		}
+
+		var marker = L.marker(L.latLng(a.latitude, a.longitude), { title: title });
+		marker.bindPopup(title);
+		markerList.push(marker);
+	}
+
+	markers.addLayers(markerList);
+	map.addLayer(markers);
+}
+
+data.on('filtered', updateClusters);
